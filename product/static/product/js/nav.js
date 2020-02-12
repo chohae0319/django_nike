ed8df81dd8c9d5be862320a4edbc1422a5883e7c
Minsoo <zlemzlem5656@naver.com>
2020-02-13 01:53:43 +0900

    add : search modal 작성

--- product/static/product/js/nav.js
+++ product/static/product/js/nav.js
@@ -1,12 +1,20 @@
-// 검색 아이콘 클릭시 인풋창 띄우기
 $(function() {
   $("#search-icon").click(function() {
     $(".hide-search").toggleClass("none-hide");
   });
 });
-//   인풋창 포커스시 모달창 띄우기
+
 $(function() {
   $("#hide-search-input").focus(function() {
     $("#hide-search-modal").toggleClass("big-modal");
+  });
+});
+
+$(function() {
+  $(".search").focus(function() {
+    $(".whole-wrapper").addClass("bg-black");
+  });
+  $(".search").blur(function() {
+    $(".whole-wrapper").removeClass("bg-black");
   });
 });
