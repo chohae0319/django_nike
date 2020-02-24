d47ee99094fdfeaf482676b4abdf9da1cf1a24d2
woogie0781 <guddnr0807@gmail.com>
2020-02-24 16:55:17 +0900

    200224

--- member/admin.py
+++ member/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from .models import Item, Profile, OrderItem, Order
 
-<<<<<<< HEAD
+
 admin.site.register(Item)
 admin.site.register(Profile)
 admin.site.register(OrderItem)
 admin.site.register(Order)
-=======
-admin.site.register(Item)
->>>>>>> 19dcab486c05c0adc0a86d81934064ac60d08f1d
+
+
