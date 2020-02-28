602f1d4e4b2f7e1f2ce8b84563ef9585acdc1cec
Minsoo <zlemzlem5656@naver.com>
2020-02-28 23:35:27 +0900

    front-8 : 배송지 불러오거나 새로 입력하게 끔 만들어서 데이터 저장 기능 구현

--- order/static/order/js/checkout-next-step.js
+++ order/static/order/js/checkout-next-step.js
@@ -1,10 +1,10 @@
-var userinfo = {
-  이름: "",
-  연락처: "",
-  주소: "",
-  메모: ""
+var userInfo = {
+  name: "",
+  phone: "",
+  address: "",
+  memo: ""
 };
-
+var nextStep = false;
 $(function() {
   $(".input-info").on("blur", function() {
     // 미입력시
@@ -97,7 +97,9 @@ $(function() {
     // 터치 이벤트 활성화
     $(".checkout-next-step").removeClass("checkout-next-step");
     // 현재 단계 자동 접음
-    $("#checkout-address-detail").removeAttr("checked");
+    $("#checkout-address-detail")
+      .removeAttr("checked")
+      .prop("checked", false);
     // input 창은 안 보이게 가려줌
     $(".input-info").attr("style", "display : none");
     // 정보 텍스트화
@@ -105,6 +107,7 @@ $(function() {
     $("#receive_phone_info").text($("#receive_phone").val());
     $("#receive_address_info").text($("#receive_address").val());
     $("#receive_memo_info").text($("#memo").val());
+    $("#after-address").text(userInfo.address);
 
     // 버튼 토글
     $(this).attr("style", "display : none");
@@ -118,4 +121,118 @@ $(function() {
   });
 });
 
+// 배송지가 있을때 다음단계 진행
+$(function() {
+  $("#checkout-next2").click(function() {
+    var alertMsg = $("#alert-msg-next");
+    if ($(`input[name="address-tab"]:checked`).val() === "newAddress") {
+      //   userinfo의 길이만큼 반복하며 입력 안 한게 있는지 검사
+      for (var i = 0; i < Object.keys(userinfo).length; i++) {
+        if (userinfo[Object.keys(userinfo)[i]] === "") {
+          alertMsg.text(Object.keys(userinfo)[i] + "을 입력해주세요");
 
+          $(`input[name=${Object.keys(userinfo)[i]}]`).focus();
+          return;
+        }
+      }
+    } else if ($(`input[name="address-tab"]:checked`).val() === "oldAddress") {
+      var number = $(".selected-address").length;
+      var memo = $("#checkout-address-memo").val();
+      if (!memo) {
+        alertMsg.text("배송 메모를 작성해주세요");
+        return;
+      }
+      if (number === 0) {
+        alertMsg.text("배송받으실 주소를 선택해주세요");
+        return;
+      } else if (number !== 1) {
+        alertMsg.text("하나의 배송지만 선택해주세요");
+        return;
+      }
+      // 다 만족했을 때
+      else {
+        alertMsg.text("");
+        userInfo = {
+          ...userInfo,
+          memo: memo
+        };
+        nextStep = true;
+        console.log(userInfo);
+        console.log(nextStep);
+      }
+    }
+    // 정보 텍스트화
+    $("#checkout-username").text(userInfo.name);
+    $("#checkout-address").text(userInfo.address);
+    $("#checkout-phone").text(userInfo.phone);
+    $("#checkout-memo").text(userInfo.memo);
+    $("#after-address").text(userInfo.address);
+    // 현재 단계 자동 접음
+    $("#checkout-address-detail")
+      .removeAttr("checked")
+      .prop("checked", false);
+    // 다음단계 활성화
+    $("#checkout-pay-detail").prop("checked", true);
+    $("#checkout-sale-detail").prop("checked", true);
+    // 터치 이벤트 활성화
+    $(".checkout-next-step").removeClass("checkout-next-step");
+    // 다음단계 버튼 삭제
+    $(this).css({ display: "none" });
+    // 입력창 지움
+    $("#input-user_address").css({ display: "none" });
+    console.log(userInfo);
+    return userInfo;
+  });
+});
+$(document).ready(function() {
+  $("#checkout-address-detail").click(function() {
+    var number = $("#checkout-address-detail:checked").length;
+    if (nextStep) {
+      if (number === 1) {
+        $("#before-next-step").css({ display: "block" });
+      } else if (number !== 1) {
+        $("#before-next-step").css({ display: "none" });
+      }
+    }
+    // 다음단계가 아니라면
+    else {
+      return;
+    }
+  });
+});
+
+$(document).ready(function() {
+  var oldtab = $("#checkout-address-tab-old");
+  var newtab = $("#checkout-address-tab-new");
+  var oldlabel = $("#checkout-address-label-old");
+  var newlabel = $("#checkout-address-label-new");
+  oldlabel.css({ backgroundColor: "white" });
+  newlabel.css({ backgroundColor: "#e0e0e0" });
+
+  // tab menu 클릭시 색상 변경
+  $(`input[name="address-tab"]`).on("click", function() {
+    var checkedRadio = $(`input[name="address-tab"]:checked`);
+    if (checkedRadio.val() === "oldAddress") {
+      oldlabel.css({ backgroundColor: "white" });
+      newlabel.css({ backgroundColor: "#e0e0e0" });
+    } else if (checkedRadio.val() === "newAddress") {
+      newlabel.css({ backgroundColor: "white" });
+      oldlabel.css({ backgroundColor: "#e0e0e0" });
+    }
+  });
+  // 클릭한 주소 테두리
+  // 클릭한 주소를 userInfo_old에 담는다.
+  $(".checkout-address-oldList").on("click", function() {
+    $(this).toggleClass("selected-address");
+    var name = $(".selected-address .receiver-name").text();
+    var address = $(".selected-address .receiver-address").text();
+    var phone = $(".selected-address .receiver-phone").text();
+    userInfo = {
+      address: address,
+      name: name,
+      phone: phone,
+      memo: ""
+    };
+    console.log(userInfo);
+  });
+});
